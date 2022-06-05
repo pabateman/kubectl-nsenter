80af6038f8d2f451b2b737fc838fbe3d412b4b7b
pabateman <ronofeedback@gmail.com>
2022-06-05 15:11:38 +0700

    goimports

--- internal/nsenter/containerinfo.go
+++ internal/nsenter/containerinfo.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
